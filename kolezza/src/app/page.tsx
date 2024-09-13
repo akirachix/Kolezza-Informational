8cc958924471f10fca1095e9ac71b21f18cadea2
queen-bellah <umurerwaqueenbella@gmail.com>
2024-09-13 09:07:41 +0300

    merged with FAQS

--- kolezza/src/app/page.tsx
+++ kolezza/src/app/page.tsx
@@ -3,7 +3,7 @@ import FaQs from "./components/Faqs";
 
   
 // import Landingpage from './Landingpage';
-import Landingpage from "./Landingpage";
+import Landingpage from "./components/Landingpage";
 export default function Home() {
   return (
     <div>
