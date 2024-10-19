b2b8d58c5b578f19f8a45ed6802b2f99da093c30
Student <christinneakinyi@gmail.com>
2024-10-19 19:35:43 +0300

    Add changes to the navbar and styling

--- kolezza/src/app/components/About/index.tsx
+++ kolezza/src/app/components/About/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const SawaTokInfo = () => {
   return (
@@ -8,23 +9,48 @@ const SawaTokInfo = () => {
       </p>
 
       <div className='flex flex-col lg:flex-row gap-8 lg:gap-28'>
-      <div className='mt-8 lg:mt-16'>
-  <img src="/images/parent.avif" alt="parents image" className='block lg:hidden w-full h-auto mb-8' />
+        <div className='mt-8 lg:mt-16'>
+       
+          <div className='block lg:hidden w-full h-auto mb-8'>
+            <Image
+              src="/images/parent.avif"
+              alt="parents image"
+              width={549}  
+              height={748} 
+              className="w-full h-auto"
+            />
+          </div>
 
-  <h2 className='text-customDarkBlue text-[24px] md:text-[36px] lg:text-[40px] is:text-[26px] mb-2 nh:text-[26px] lg:mb-10 font-nunito antialiased font-bold drop-shadow-[3px_3px_6px_rgba(0,0,0,0.1)]'>
-    An outstanding fluency journey
-  </h2>
-  <p className='text-[16px] md:text-[24px] lg:text-[28px] nh:text-[24px] font-nunito'>
-    Sawatok has different modules that improve the communication abilities of kids. The modules contain images and text that the kids should say out loud.
-    After that, the kids are awarded points according to the improvement they have made so far.
-  </p>
+          <h2 className='text-customDarkBlue text-[24px] md:text-[36px] lg:text-[40px] is:text-[26px] mb-2 nh:text-[26px] lg:mb-10 font-nunito antialiased font-bold drop-shadow-[3px_3px_6px_rgba(0,0,0,0.1)]'>
+            An outstanding fluency journey
+          </h2>
+          <p className='text-[16px] md:text-[24px] lg:text-[28px] nh:text-[24px] font-nunito'>
+            Sawatok has different modules that improve the communication abilities of kids. The modules contain images and text that the kids should say out loud.
+            After that, the kids are awarded points according to the improvement they have made so far.
+          </p>
 
-  <img src="/images/parent.avif" alt="parents image" className='hidden lg:block w-[549px] l-[748px] lg:w-[427px] h-auto mt-16 lg:mt-20 lg:ml-14' /> {/* Added lg:ml-14 */}
-</div>
-
+        
+          <div className='hidden lg:block w-[549px] l-[748px] lg:w-[427px] h-auto mt-16 lg:mt-20 lg:ml-14'>
+            <Image
+              src="/images/parent.avif"
+              alt="parents image"
+              width={427}  
+              height={748} 
+              className="h-auto"
+            />
+          </div>
+        </div>
 
         <div className='mt-10 lg:mt-10'>
-          <img src="/images/child.png" alt="child" className='w-full lg:w-[427px] h-auto ml-0 lg:ml-14 md:float-start' />
+          <div className='w-full lg:w-[427px] h-auto ml-0 lg:ml-14 md:float-start'>
+            <Image
+              src="/images/child.png"
+              alt="child"
+              width={427} 
+              height={427} 
+              className='w-full h-auto'
+            />
+          </div>
           <div className='mt-6 lg:mt-[34rem]'>
             <h1 className='text-customGreen text-[24px] md:text-[36px] is:text-[26px] lg:text-[40px] nh:text-[26px] mb-2 lg:mb-10 font-nunito font-bold drop-shadow-[3px_3px_6px_rgba(0,0,0,0.1)]'>
               Unlock your child&apos;s full potential<span className='text-customDarkBlue'> in speaking fluently.</span>
@@ -41,3 +67,4 @@ const SawaTokInfo = () => {
 };
 
 export default SawaTokInfo;
+
